8b4c598734e4124fe984f246987843f19e5dd7bd
Aamir shaikh <aamirshaikh3232@gmail.com>
2020-07-12 12:17:16 +0530

    Installing axios

--- src/App.vue
+++ src/App.vue
@@ -26,25 +26,7 @@ export default {
 
   data() {
     return {
-      todos : [
-        {
-          id: 1,
-          title: "Todo one",
-          completed: false
-        },
-
-        {
-          id: 2,
-          title: "Todo two",
-          completed: false
-        },
-
-        {
-          id: 3,
-          title: "Todo three",
-          completed: false
-        }
-      ]
+      todos : []
     }
   },
 
