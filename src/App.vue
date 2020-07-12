2d25a24d6dcaf05ea1b49d7b9c9a16cbc4ac2c70
Aamir shaikh <aamirshaikh3232@gmail.com>
2020-07-12 12:30:10 +0530

    Making GET and POST request to JSONPlaceholder

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -36,8 +37,23 @@ export default {
     },
 
     addTodo(newTodo) {
-        this.todos = [...this.todos, newTodo];
+        const { title, completed } = newTodo;
+
+        axios.post('https://jsonplaceholder.typicode.com/todos', {
+            title,
+            completed
+        })
+            .then(res => this.todos = [...this.todos, res.data])
+
+            .catch(err => console.log(err));
     }
+  },
+
+  created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(res => this.todos = res.data)
+
+        .catch(err => console.log(err));
   }
 }
 </script>
